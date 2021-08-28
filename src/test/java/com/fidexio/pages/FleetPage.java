8e88a7f523ed6b6730ff9619a9db54a58465fa8e
buckyCT <byucedal123@gmail.com>
2021-08-28 18:07:13 +0300

    Completed US54

--- src/test/java/com/fidexio/pages/FleetPage.java
+++ src/test/java/com/fidexio/pages/FleetPage.java
@@ -10,4 +10,13 @@ public class FleetPage extends BasePage{
     @FindBy(xpath = "//div[@class='oe_secondary_menu'][@data-menu-parent='134']/ul/li")
     public List<WebElement> menusOnLeft;
 
+    @FindBy(xpath = "//button[@accesskey='c'][contains(text(),'Create')]")
+    public WebElement createBtn;
+
+    @FindBy(xpath = "//button[contains(text(),'Save')][@accesskey='s']")
+    public WebElement saveBtn;
+
+    @FindBy(xpath = "//button[contains(text(),'Edit')]")
+    public WebElement editBtn;
+
 }
